c29566c68ec59b0715e69955f57e93bbebda6592
yellowei <hw0521@vip.qq.com>
2018-09-23 19:22:24 +0800

    target->static lib

---
+++ CYMediaStaticLib/CYMediaStaticLib.h
@@ -0,0 +1,13 @@
+//
+//  CYMediaStaticLib.h
+//  CYMediaStaticLib
+//
+//  Created by 黄威 on 2018/9/23.
+//  Copyright © 2018年 Sutan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CYMediaStaticLib : NSObject
+
+@end
---
+++ CYMediaStaticLib/CYMediaStaticLib.m
@@ -0,0 +1,13 @@
+//
+//  CYMediaStaticLib.m
+//  CYMediaStaticLib
+//
+//  Created by 黄威 on 2018/9/23.
+//  Copyright © 2018年 Sutan. All rights reserved.
+//
+
+#import "CYMediaStaticLib.h"
+
+@implementation CYMediaStaticLib
+
+@end
